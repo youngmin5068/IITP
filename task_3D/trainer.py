4d827ed6fcabe8094d4f844ec3b83f3abdd3b628
youngmin <youngmin5068@naver.com>
2023-09-06 07:24:09 +0000

    trainer 수정, label 픽셀 normalization

--- task_3D/trainer.py
+++ task_3D/trainer.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from util import normalize_minmax
 from config import *
 from losses import BCEDiceLoss
-from monai.losses import DiceCELoss
+from monai.losses import DiceCELoss,DiceLoss
 from monai.transforms import AsDiscrete
 from monai.data import ThreadDataLoader
 from monai.data.utils import pad_list_data_collate 
@@ -13,30 +13,28 @@ from monai.data.utils import pad_list_data_collate
 def train(train_dataset,net,optimizer,device,epoch,scheduler=None):
     net.train()
     post_label = AsDiscrete(threshold=0.5) #threshold
-    train_loader = ThreadDataLoader(train_dataset, num_workers=0, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader = ThreadDataLoader(train_dataset, num_workers=0, batch_size=BATCH_SIZE, shuffle=True,collate_fn= pad_list_data_collate)
     criterion = BCEDiceLoss(alpha=1.0,beta=1.0)
-    scaler = torch.cuda.amp.GradScaler()
-
     i=0
     for batch in train_loader:
 
         imgs = batch['image'].to(device=device)
-        true_masks = batch['label'].to(device=device)
-        masks = post_label(true_masks) #TRUE Target
+        masks = batch['label'].to(device=device)
+        scaled_image = masks / 255.0
+        true_masks = post_label(scaled_image) #TRUE Target 0 or 1
 
-        with torch.cuda.amp.autocast():
-            preds = net(imgs)
-            loss = criterion(preds, masks)
-
-        scaler.scale(loss).backward()
-        scaler.unscale_(optimizer)
-        scaler.step(optimizer)
-        scaler.update()
         optimizer.zero_grad()
+
+        preds = net(imgs)
+
+        loss = criterion(preds, true_masks)
+
+        loss.backward()
+        optimizer.step()
 
         nn.utils.clip_grad_value_(net.parameters(),0.1)
 
-        if BATCH_SIZE*i % 20== 0 :
+        if BATCH_SIZE*i % 50== 0 :
             print('epoch : {}, index : {}/{}, loss (batch) : {:.4f}'.format(
                                                                             epoch+1, 
                                                                             (i+1)*BATCH_SIZE,
