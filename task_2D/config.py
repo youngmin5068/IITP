736c6c25ad3d7c3c7ef69b129074dd145a63ab04
youngmin <youngmin5068@naver.com>
2023-09-06 07:21:53 +0000

    2d task config

---
+++ task_2D/config.py
@@ -0,0 +1,10 @@
+BATCH_SIZE = 16 # dataloader batch size 
+NUM_SAMPLES = 4 # RandCropByPosNegLabeld in training
+NUM_SLIDING = 8 # slidng batch size in validation 
+EPOCH = 1000
+MODEL_SEED = 76075154
+TRAIN_RATIO = 0.9  # 훈련 세트의 비율
+IMAGE_SIZE = (256,256)
+PIX_DIM = (1.0,1.0)
+AXCODES ="LP"
+LEARNING_RATE = 1e-4
