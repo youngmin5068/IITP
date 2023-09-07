06dda6241e086af6fb9badcd49f3772ce125c20b
youngmin <youngmin5068@naver.com>
2023-09-07 08:04:36 +0000

    train, validation main에 합치기

--- task_3D/main.py
+++ task_3D/main.py
@@ -1,23 +1,24 @@
 import os
 import torch
+import numpy as np
 import torch.nn as nn
 import logging
-import torchio as tio
 import torch.optim as optim
-from trainer import train
-from validation import validation
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
+# from trainer import train
+# from validation import validation
+# from torch.utils.data import random_split
 from dataset import data_dicts
 from util import set_seed
 from config import *
-import torchio as tio
-import logging
-from config import *
 from losses import BCEDiceLoss
 from monai.networks.nets import SwinUNETR
-from monai.data import CacheDataset
+from monai.data import decollate_batch, DataLoader, Dataset
+from monai.data.utils import pad_list_data_collate
 from transforms import train_transforms,val_transforms
+from torch.utils.tensorboard import SummaryWriter
+from monai.inferers import sliding_window_inference
+from monai.transforms import AsDiscrete, Activations
+from monai.metrics import DiceMetric
 
 def main(net,device):
 
@@ -27,65 +28,141 @@ def main(net,device):
     val_label_paths = "/workspace/breast_mri/tumor_tuning_3d/tumor"
     dir_checkpoint = "/workspace/IITP/task_3D/dir_checkpoint"
 
-    # dicts = data_dicts(train_image_paths,train_label_paths)
-    # train_dicts = dicts[:int(len(dicts)*0.95)]
-    # val_dicts = dicts[int(len(dicts)*0.95):]
+    #dicts = data_dicts(train_image_paths,train_label_paths)
+    # train_dicts = dicts[370:]
+    # val_dicts = dicts[0:50]
+    # train_dicts = dicts[:int(len(dicts)*0.9)]
+    # val_dicts = dicts[int(len(dicts)*0.9):]
 
 
-    train_dataset = CacheDataset(data=data_dicts(train_image_paths,train_label_paths),
+    train_dataset = Dataset(data=data_dicts(train_image_paths,train_label_paths),
                             transform = train_transforms,
-                            cache_num = 24,
-                            cache_rate=1.0,
-                            num_workers=8)
+                            # cache_num=24,
+                            # cache_rate=1.0,
+                            # num_workers=8
+                            )
     
-    val_dataset = CacheDataset(data=data_dicts(val_image_paths,val_label_paths),
+    val_dataset = Dataset(data=data_dicts(val_image_paths,val_label_paths),
                             transform = val_transforms,
-                            cache_num = 6,
-                            cache_rate=1.0,
-                            num_workers=4)
+                            # cache_num=8,
+                            # cache_rate=1.0,
+                            # num_workers=4
+                        )
+    
+    train_loader = DataLoader(train_dataset, num_workers=16, batch_size=BATCH_SIZE, shuffle=True,pin_memory=True,collate_fn=pad_list_data_collate)
+    val_loader =  DataLoader(val_dataset, num_workers=16, batch_size=BATCH_SIZE, shuffle=False,pin_memory=True,collate_fn=pad_list_data_collate)
+    
+    logging.basicConfig(level=logging.INFO)
 
     logging.info(f'''Starting training:
         Epochs:          {EPOCH}
         Batch size:      {BATCH_SIZE}
         Train size:      {len(train_dataset)}
         Test size:       {len(val_dataset)}
-        Learning rate:   {LearningRate}        
+        Learning rate:   {LEARNING_RATE}        
         Device:          {device}
     ''')
 
-    optimizer = optim.AdamW(net.parameters(),betas=(0.9,0.999),lr=LearningRate,weight_decay=1e-4) # weight_decay : prevent overfitting
-    #scheduler = optim.lr_scheduler.MultiStepLR(optimizer,milestones=[100,150],gamma=0.1)
-    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=100,T_mult=1,eta_min=0.000001,last_epoch=-1)
+    optimizer = torch.optim.AdamW(net.parameters(), lr=LEARNING_RATE, weight_decay=1e-5) # weight_decay : prevent overfitting
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer,milestones=[200],gamma=0.1)
+    #scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=10,T_mult=1,eta_min=1e-5,last_epoch=-1)
     
     best_dice = 0.0
     best_epoch = 1
 
-    for epoch in range(EPOCH):
-        train(train_dataset,net=net,optimizer=optimizer,epoch=epoch,device=device)
-        print("----------validation start----------")
-        dice_score = validation(val_dataset=val_dataset,net=net,device=device)
+    validation_interval = 10
+    best_dice = 0.0
+    best_epoch = 1
 
-        if dice_score > best_dice:
-            best_dice = dice_score
+    post_label = AsDiscrete(threshold=0.5) #threshold
+    post_pred = AsDiscrete(threshold=0.5)
+    diceCoeff = DiceMetric(include_background=True,reduction="mean")
+    
+    post_sigmoid = Activations(sigmoid=True)
+    criterion = BCEDiceLoss(alpha=1.0,beta=1.0)
+    losses = []
+    dices = []
+    for epoch in range(EPOCH):
+        #train(train_loader,net=net,optimizer=optimizer,epoch=epoch,device=device)
+        print("----------train start----------")
+        net.train()
+        i=0
+        for batch in train_loader:
+
+            imgs = batch['image'].to(device=device)
+            masks = batch['label'].to(device=device)
+            scaled_image = masks / 255.0
+            true_masks = post_label(scaled_image) #TRUE Target 0 or 1
+
+            optimizer.zero_grad()
+
+            preds = net(imgs)
+            loss = criterion(preds, true_masks)
+            
+            loss.backward()
+            optimizer.step()
+
+            nn.utils.clip_grad_value_(net.parameters(),0.1)
+
+            if BATCH_SIZE*i % 100 == 0 :
+                print('epoch : {}, index : {}/740, loss (batch) : {:.4f}'.format(
+                                                                                epoch+1, 
+                                                                                (i+1)*BATCH_SIZE,
+                                                                                loss.detach())
+                                                                                )
+                losses.append(loss.detach())
+            i += 1
+
+        #if (epoch+1) % validation_interval:
+        print("----------validation start----------")
+            #dice_score = validation(val_loader,net=net,device=device)
+        net.eval()
+
+        with torch.no_grad():
+            for batch in val_loader:
+                imgs = batch['image'].to(device=device)
+                true_masks = batch["label"].to(device=device)
+                with torch.cuda.amp.autocast():
+                    val_outputs = sliding_window_inference(imgs,IMAGE_SIZE, NUM_SLIDING, net)
+                val_labels_list = decollate_batch(true_masks)
+                val_labels_convert = [post_label(val_label_tensor/255.0) for val_label_tensor in val_labels_list]
+
+                #val_outputs = net(imgs)
+                val_outputs_list = decollate_batch(val_outputs) 
+                val_output_convert = [post_pred(post_sigmoid(val_pred_tensor)) for val_pred_tensor in val_outputs_list]
+
+                diceCoeff(y_pred=val_output_convert, y=val_labels_convert)
+                
+            mean_dice_val = diceCoeff.aggregate().item()
+            dices.append(mean_dice_val)
+            diceCoeff.reset()
+
+        print("mean_dice_val : {:.4f}".format(mean_dice_val))
+        
+        if mean_dice_val > best_dice:
+            best_dice = mean_dice_val
             best_epoch = epoch+1
             try:
                 os.mkdir(dir_checkpoint)
                 logging.info("Created checkpoint directory")
             except OSError :
                 pass
-        torch.save(net.state_dict(), dir_checkpoint+ "/just_testing.pth")
-        logging.info(f'Checkpoint {epoch + 1} saved !')
-
-        print("epoch : {} , best_dice : {:.4f}".format(best_epoch, best_dice))
+            torch.save(net.state_dict(), dir_checkpoint+ "/best_model_2023_09_07.pth")
+            logging.info(f'Checkpoint {epoch + 1} saved !')
+        print("BEST EPOCH : {} , BEST_DICE : {:.4f}".format(best_epoch, best_dice))
+            
         scheduler.step()
+    return losses,dices
+
 
 
  
 if __name__ == "__main__":
 
     set_seed(MODEL_SEED)
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     torch.backends.cudnn.benchmark = True
-    net = SwinUNETR(img_size=IMAGE_SIZE,spatial_dims=len(IMAGE_SIZE),in_channels=1,out_channels=1,depths=(2,2,2,2))
+    net = SwinUNETR(img_size=IMAGE_SIZE,spatial_dims=len(IMAGE_SIZE),in_channels=1,out_channels=1)
     device = torch.device(f'cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
 
@@ -94,4 +171,7 @@ if __name__ == "__main__":
         print("data parallel ON")
     net.to(device=device)
 
-    main(net=net,device=device)
+    losses, dices = main(net=net,device=device)
+
+    np.save("/workspace/IITP/task_3D/dir_checkpoint/swinunetr_losses.npy",losses)
+    np.save("/workspace/IITP/task_3D/dir_checkpoint/swinunetr_dices.npy",dices)
