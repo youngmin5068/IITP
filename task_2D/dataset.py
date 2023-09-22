806e3c1f5f94a48a0c3c1ed6fa3d2e3c12a22fbb
youngmin <youngmin5068@naver.com>
2023-09-22 01:25:55 +0000

    GMIC_UNET을 위한 dataset 수정

--- task_2D/dataset.py
+++ task_2D/dataset.py
@@ -43,9 +43,9 @@ class lung_Dataset(Dataset):
         self.transform = transforms.Compose([transforms.ToTensor(),
                                             transforms.Resize((512,512)),
                                             #transforms.CenterCrop(size=384),
-                                            #customRandomHorizontalFlip(SEED=idx,p=0.5), 
+                                            customRandomHorizontalFlip(SEED=idx,p=0.5), 
                                             customRandomRotate(degrees=180,SEED=idx),
-                                            customRandomResizedCrop(SEED=idx,size=(256,256)),
+                                            #customRandomResizedCrop(SEED=idx,size=(384,384)),
                                              ])
         
         image_path = self.train_path_list[idx]
@@ -55,7 +55,7 @@ class lung_Dataset(Dataset):
         min_val = np.min(image)
         max_val = np.max(image)
         image = (image - min_val) / (max_val - min_val)
-        #image = apply_voi_lut(image, slice)
+        image = apply_voi_lut(image, slice)
         image = Image.fromarray(image)
 
         label_path = self.label_path_list[idx]
@@ -85,7 +85,7 @@ class breast_Dataset(Dataset):
         self.label_list = []
 
         self.train_path = path + "/input_dcm"
-        self.label_path = path + "/breast"
+        self.label_path = path + "/target"
 
         
         for file in os.listdir(self.train_path):
@@ -99,7 +99,13 @@ class breast_Dataset(Dataset):
 
     def __len__(self):
         return len(self.label_path_list)
-        
+    
+    def get_label(self,image):
+     if np.any(image == 1):
+        return 1
+     else:
+        return 0
+
     def __getitem__(self,idx):
         if self.train:
             self.transform = transforms.Compose([transforms.ToTensor(),
@@ -132,33 +138,26 @@ class breast_Dataset(Dataset):
         thresh = np.zeros_like(target_image)
         thresh[target_image > 0.5] = 1
 
-        return input_image, thresh
+        class_label = self.get_label(thresh)
+
+        return input_image, thresh, torch.from_numpy(class_label)
 
 class tumor_Dataset(Dataset):
-    def __init__(self,path):
-        self.path = path
+    def __init__(self,root,input_path,target_path):
+        self.path = root
 
         self.train_path_list = []
         self.train_list = []
 
-        self.label_path_list = []
-        self.label_list = []
-
         self.target_path_list = []
         self.target_list = []
 
-        self.train_path = path + "/input"
-        #self.label_path = path + "/breast"
-
-        self.target_path = path + "/breast"
+        self.train_path = root + input_path
+        self.target_path = root + target_path
         
         for file in os.listdir(self.train_path):
             self.train_path_list.append(os.path.join(self.train_path,file))
         self.train_path_list.sort()
-                
-        # for file in os.listdir(self.label_path):
-        #     self.label_path_list.append(os.path.join(self.label_path,file))           
-        # self.label_path_list.sort()
 
         for file in os.listdir(self.target_path):
             self.target_path_list.append(os.path.join(self.target_path,file))           
@@ -167,18 +166,20 @@ class tumor_Dataset(Dataset):
     def __len__(self):
         return len(self.target_path_list)
         
+    def get_label(self,image):
+        if np.any(image == 1):
+            return 1
+        else:
+            return 0
+        
     def __getitem__(self,idx):
-
-        mean = [0, ] # 예시 값
-        std = [1, ]  # 예시 값
-
         self.transform = transforms.Compose([transforms.ToTensor(),
                                             transforms.Resize((512,512)),
-                                            customRandomRotate(degrees=180,SEED=idx),
-                                            customRandomResizedCrop(SEED=idx,size=(128,128))
+                                            #customRandomRotate(degrees=180,SEED=idx),
+                                            customRandomHorizontalFlip(SEED=idx,p=0.5),
+                                            customRandomVerticalFlip(SEED=idx,p=0.5)
+                                            #customRandomResizedCrop(SEED=idx,size=(256,256))
                                              ])
-        normalized = transforms.Normalize(mean=mean,std=std)
-        
         image_path = self.train_path_list[idx]
 
         slice = dcm.read_file(image_path)
@@ -190,27 +191,18 @@ class tumor_Dataset(Dataset):
         image = (image - min_val) / (max_val - min_val+epsilon)
         image = Image.fromarray(image)
 
-        # label_path = self.label_path_list[idx]
-        # label = np.array(Image.open(label_path).convert("L"))
-
-        # label = Image.fromarray(label)
-
         target_path = self.target_path_list[idx]
         target = np.array(Image.open(target_path).convert("L"))
 
         target = Image.fromarray(target)
 
         input_image = self.transform(image)
-        #input_image = normalized(input_image)
-        #label_image = self.transform(label)
         target_image = self.transform(target)
 
-        # thresh = np.zeros_like(label_image)
-        # thresh[label_image > 0.5] = 1
 
         target_thresh = np.zeros_like(target_image)
         target_thresh[target_image > 0.5] =1
 
-        #input_img = input_image*thresh
-
-        return input_image, target_thresh
+        class_label = float(self.get_label(target_thresh))
+        
+        return input_image, target_thresh, class_label
