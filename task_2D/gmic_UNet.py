ccd57e796039c1faacc1bd3d9c1cbb9533d912a0
youngmin <youngmin5068@naver.com>
2023-09-22 01:30:34 +0000

    gmic_UNet 모델

---
+++ task_2D/gmic_UNet.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import tools 
+import modules as m
+import torch.nn.functional as F
+from monai.networks.nets import SwinUNETR
+
+
+class GMIC_UNet(nn.Module):
+    def __init__(self, parameters):
+        super(GMIC_UNet, self).__init__()
+
+        self.swin_unetr = SwinUNETR(img_size=(512,512),spatial_dims=2,in_channels=1,out_channels=1,depths=(2,4,2,2))
+
+        # save parameters
+        self.experiment_parameters = parameters
+
+        self.global_network = m.GlobalNetwork(self.experiment_parameters)
+        self.aggregation_function = m.TopTPercentAggregationFunction(self.experiment_parameters)
+    def forward(self,x_original):
+
+        swin_output = self.swin_unetr(x_original)
+        h_g, self.saliency_map = self.global_network(x_original)
+        saliency_resized = F.interpolate(self.saliency_map, size=(512,512),mode="bilinear",align_corners=True)
+        
+        self.y_global = self.aggregation_function(self.saliency_map)
+        
+        att_result = swin_output * saliency_resized
+
+        return att_result, self.y_global, saliency_resized
+    
+# sample = torch.randn((1,1,512,512))
+
+# parameters = {
+#         "percent_t":0.1,
+#         # "device_type":"gpu",
+#         # "gpu_number":0,
+#         "post_processing_dim": 256,
+#         "num_classes": 1
+#     }
+
+# model = GMIC_UNet(parameters)
+
+# print(torch.max(model(sample)[2]))
