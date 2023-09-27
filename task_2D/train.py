a97ae388e13678d8acd01e26d1949e3643a5679f
youngmin <youngmin5068@naver.com>
2023-09-27 04:23:02 +0000

    refactor : train 코드 수정

--- task_2D/train.py
+++ task_2D/train.py
@@ -16,6 +16,8 @@ from metric import *
 from monai.networks.nets import SwinUNETR
 from Model.MKA.LK_PC import LK_PC_UNet
 from gmic_UNet import GMIC_UNet
+from modified_PCCA_UNET import pcca_UNet
+from topTcbam_UNet import top_t_cbam_UNet
 
 def set_seed(seed):
     torch.manual_seed(seed)
@@ -30,7 +32,7 @@ def set_seed(seed):
 def train_net(net,                       
               device,     
               epochs=100,
-              batch_size=32,
+              batch_size=16,
               lr=0.001,
               save_cp=True
               ):
@@ -38,7 +40,7 @@ def train_net(net,
     dataset_path = "/mount_folder"
 
     dataset = tumor_Dataset(dataset_path,"/input_dcm","/breast_mask")
-    train_ratio = 0.9
+    train_ratio = 0.95
     train_length = int(train_ratio * len(dataset))
     test_length = len(dataset) - train_length
 
@@ -60,8 +62,8 @@ def train_net(net,
     ''')    
 
     optimizer = optim.AdamW(net.parameters(),betas=(0.9,0.999),lr=lr,weight_decay=1e-4) # weight_decay : prevent overfitting
-    #scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=10,T_mult=1,eta_min=0.00001,last_epoch=-1)
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer,milestones=[25],gamma=0.1)
+    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=5,T_mult=2,eta_min=0.00001,last_epoch=-1)
+    #scheduler = optim.lr_scheduler.MultiStepLR(optimizer,milestones=[50],gamma=0.1)
     diceloss = DiceLoss()
     bceloss = nn.BCEWithLogitsLoss()
     classifyloss = nn.BCELoss()
@@ -76,37 +78,34 @@ def train_net(net,
 
         net.train()
         i=1
-        for imgs,true_masks,true_class in train_loader:
+        for imgs,true_masks,_ in train_loader:
 
             imgs = imgs.to(device=device,dtype=torch.float32)
             true_masks = true_masks.to(device=device,dtype=torch.float32)
-            true_class = true_class.to(device=device,dtype=torch.float32).unsqueeze(1)
+            #true_class = true_class.to(device=device,dtype=torch.float32).unsqueeze(1)
             #optimizer.zero_grad()
             for param in net.parameters():
                 param.grad = None
 
-            masks_preds,cl_preds,saliency_map = net(imgs)
-
+            #masks_preds,cl_preds,saliency_map = net(imgs)
+            masks_preds = net(imgs)
             loss1 = diceloss(torch.sigmoid(masks_preds),true_masks)
             loss2 = bceloss(masks_preds,true_masks)
-            loss3 = classifyloss(saliency_map,true_masks)
 
-            loss = loss1+loss2+loss3
+            loss = loss1+loss2
             loss.backward()
 
             nn.utils.clip_grad_value_(net.parameters(), 0.1)     
 
             optimizer.step()
 
-            
             if i*batch_size%800 == 0:
-                print('epoch : {}, index : {}/{},dice loss : {:.4f}, bce loss : {:.4f}, saliency loss : {:.4f}, loss (batch) : {:.4f}'.format(
+                print('epoch : {}, index : {}/{},dice loss : {:.4f}, bce loss : {:.4f}, loss (batch) : {:.4f}'.format(
                                                                                                                         epoch+1, 
                                                                                                                         i*batch_size,
                                                                                                                         len(train_dataset),
                                                                                                                         loss1.detach(),
                                                                                                                         loss2.detach(),
-                                                                                                                        loss3.detach(),
                                                                                                                         loss.detach())) 
             i += 1
 
@@ -121,7 +120,7 @@ def train_net(net,
             imgs = imgs.to(device=device,dtype=torch.float32)
             true_masks = true_masks.to(device=device,dtype=torch.float32)
             with torch.no_grad():
-                mask_pred,_,_ = net(imgs)
+                mask_pred = net(imgs)
                 mask_pred = torch.sigmoid(mask_pred)
 
             thresh = torch.zeros_like(mask_pred)
@@ -134,7 +133,7 @@ def train_net(net,
 
         print("dice score : {:.4f}, len(val_loader) : {:.4f}".format(dice, len(val_loader)))
         print("dice score : {:.4f}, recall score : {:.4f}, precision score : {:.4f}".format(dice/len(val_loader), recall/len(val_loader),precision/len(val_loader)) )
-        #scheduler.step()
+        scheduler.step()
         
         if dice/len(val_loader) > best_dice:
             best_dice = dice/len(val_loader)
@@ -148,7 +147,7 @@ def train_net(net,
                     logging.info("Created checkpoint directory")
                 except OSError:
                     pass
-                torch.save(net.state_dict(), dir_checkpoint + f'/GMIC_SWINUNETR.pth')
+                torch.save(net.state_dict(), dir_checkpoint + f'/top_t_cbam_UNet2.pth')
                 
                 logging.info(f'Checkpoint {epoch + 1} saved !')
 
@@ -156,24 +155,27 @@ def train_net(net,
         
 
 if __name__ == '__main__':
-    Model_SEED = 980118
+    Model_SEED = 7777777
     set_seed(Model_SEED)
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
     
-    parameters = {
-            "percent_t":0.1,
-            "device_type":"gpu",
-            "gpu_number":2,
-            "post_processing_dim": 256,
-            "num_classes": 1
-        }
+    # parameters = {
+    #         "percent_t":0.1,
+    #         "device_type":"gpu",
+    #         "gpu_number":2,
+    #         "post_processing_dim": 256,
+    #         "num_classes": 1
+    #     }ㅜ
 
-    net = GMIC_UNet(parameters).to(device=device)
+    # net = GMIC_UNet(parameters).to(device=device)
+
+    #net = pcca_UNet(1,1).to(device=device)
+    net = top_t_cbam_UNet(1,1,percent_t=0.3).to(device=device)
     if torch.cuda.device_count() > 1:
-        net = nn.DataParallel(net,device_ids=[0,1]) 
+        net = nn.DataParallel(net,device_ids=[0,1,2,4]) 
 
     train_net(net=net,device=device)
 
