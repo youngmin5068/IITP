05e08876d7a402f665bf54782a67c0b3ab4ae3c8
youngmin <youngmin5068@naver.com>
2023-10-05 00:25:50 +0000

    trained model 통한 breast 영역 추출 및 tumor segmentation

--- task_2D/train.py
+++ task_2D/train.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torchio as tio
 from custom_transforms import *
-from dataset import breast_Dataset,tumor_Dataset,lung_Dataset
+from dataset import tumor_Dataset
 from Model.AAUNET.AAU_Net import AAU_Net
 from Model.TRANSUNET.transunet import TransUNet
 from loss import *
@@ -29,28 +29,28 @@ def set_seed(seed):
 
 
 
-def train_net(net,                       
+def train_net(net,
+              roi_model,                 
               device,     
               epochs=100,
               batch_size=16,
               lr=0.001,
               save_cp=True
               ):
-    dir_checkpoint = '/workspace/IITP/task_2D/dir_checkpoint_breast_ROI'
-    dataset_path = "/mount_folder"
+    dir_checkpoint = '/workspace/IITP/task_2D/dir_checkpoint_tumorResult'
+    dataset_path = "/mount_folder/sampling"
 
-    dataset = tumor_Dataset(dataset_path,"/input_dcm","/breast_mask")
-    train_ratio = 0.95
-    train_length = int(train_ratio * len(dataset))
-    test_length = len(dataset) - train_length
+    dataset = tumor_Dataset(dataset_path)
+    train_size = int(0.8 * len(dataset))
+    val_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - val_size  # 나머지 샘플은 test 세트에 할당됩니다.
 
-    train_dataset, test_dataset = random_split(dataset, [train_length, test_length])
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
     
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,num_workers=12,pin_memory=True)
-    val_loader = DataLoader(test_dataset, batch_size=batch_size,shuffle=False,num_workers=12,pin_memory=True)
-
+    val_loader = DataLoader(val_dataset, batch_size=batch_size,shuffle=False,num_workers=12,pin_memory=True)
+    test_loader = DataLoader(test_dataset,batch_size=batch_size,shuffle=False,num_workers=12,pin_memory=True)
     
-
     logging.info(f'''Starting training:
         Epochs:          {epochs}
         Batch size:      {batch_size}
@@ -62,7 +62,7 @@ def train_net(net,
     ''')    
 
     optimizer = optim.AdamW(net.parameters(),betas=(0.9,0.999),lr=lr,weight_decay=1e-4) # weight_decay : prevent overfitting
-    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=5,T_mult=2,eta_min=0.00001,last_epoch=-1)
+    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=10,T_mult=2,eta_min=0.00001,last_epoch=-1)
     #scheduler = optim.lr_scheduler.MultiStepLR(optimizer,milestones=[50],gamma=0.1)
     diceloss = DiceLoss()
     bceloss = nn.BCEWithLogitsLoss()
@@ -73,26 +73,34 @@ def train_net(net,
     best_precision = 0.0
     best_recall = 0.0
 
-    
     for epoch in range(epochs):
 
         net.train()
+        roi_model.eval()
         i=1
-        for imgs,true_masks,_ in train_loader:
+        for imgs,true_masks in train_loader:
 
             imgs = imgs.to(device=device,dtype=torch.float32)
             true_masks = true_masks.to(device=device,dtype=torch.float32)
-            #true_class = true_class.to(device=device,dtype=torch.float32).unsqueeze(1)
+            # true_class = true_class.to(device=device,dtype=torch.float32).unsqueeze(1)
             #optimizer.zero_grad()
             for param in net.parameters():
                 param.grad = None
 
             #masks_preds,cl_preds,saliency_map = net(imgs)
-            masks_preds = net(imgs)
-            loss1 = diceloss(torch.sigmoid(masks_preds),true_masks)
-            loss2 = bceloss(masks_preds,true_masks)
 
-            loss = loss1+loss2
+            with torch.no_grad():
+                roi_preds = torch.sigmoid(roi_model(imgs))
+                roi_thresh = torch.zeros_like(roi_preds)
+                roi_thresh[roi_preds>0.5] = 1.0
+                roi_results = imgs * roi_thresh
+            masks_preds = net(roi_results)
+
+            loss1 = diceloss(torch.sigmoid(masks_preds),true_masks)
+            #loss1 = bceloss(masks_preds,true_masks)
+            #loss2 = bceloss(masks_preds,true_masks)
+
+            loss = loss1
             loss.backward()
 
             nn.utils.clip_grad_value_(net.parameters(), 0.1)     
@@ -100,13 +108,11 @@ def train_net(net,
             optimizer.step()
 
             if i*batch_size%800 == 0:
-                print('epoch : {}, index : {}/{},dice loss : {:.4f}, bce loss : {:.4f}, loss (batch) : {:.4f}'.format(
-                                                                                                                        epoch+1, 
-                                                                                                                        i*batch_size,
-                                                                                                                        len(train_dataset),
-                                                                                                                        loss1.detach(),
-                                                                                                                        loss2.detach(),
-                                                                                                                        loss.detach())) 
+                print('epoch : {}, index : {}/{}, dice loss : {:.4f}'.format(
+                                                                                epoch+1, 
+                                                                                i*batch_size,
+                                                                                len(train_dataset),
+                                                                                loss1.detach())) 
             i += 1
 
         #when train epoch end
@@ -116,15 +122,21 @@ def train_net(net,
         recall = 0.0
         precision = 0.0
 
-        for imgs, true_masks,_ in val_loader:
+        for imgs, true_masks in val_loader:
             imgs = imgs.to(device=device,dtype=torch.float32)
             true_masks = true_masks.to(device=device,dtype=torch.float32)
             with torch.no_grad():
-                mask_pred = net(imgs)
+                roi_preds = torch.sigmoid(roi_model(imgs))
+                roi_thresh = torch.zeros_like(roi_preds)
+                roi_thresh[roi_preds>0.5] = 1.0
+
+                roi_results = imgs * roi_thresh
+
+                mask_pred = net(roi_results)
                 mask_pred = torch.sigmoid(mask_pred)
 
             thresh = torch.zeros_like(mask_pred)
-            thresh[mask_pred > 0.5] = 1
+            thresh[mask_pred > 0.5] = 1.0
 
             precision += precision_score(thresh,true_masks)
             recall += recall_score(thresh,true_masks)
@@ -147,11 +159,39 @@ def train_net(net,
                     logging.info("Created checkpoint directory")
                 except OSError:
                     pass
-                torch.save(net.state_dict(), dir_checkpoint + f'/top_t_cbam_UNet2.pth')
+                torch.save(net.state_dict(), dir_checkpoint + f'/top_t_01_cbam_10_05.pth')
                 
                 logging.info(f'Checkpoint {epoch + 1} saved !')
 
         print("epoch : {} , best_dice : {:.4f}, best_recall : {:.4f}, best_precision : {:.4f}".format(best_epoch, best_dice,best_recall,best_precision))
+
+    print("--------------------------TEST------------------------")
+    precision=0.0
+    recall=0.0
+    dice=0.0
+    for imgs, true_masks in test_loader:
+        imgs = imgs.to(device=device,dtype=torch.float32)
+        true_masks = true_masks.to(device=device,dtype=torch.float32)
+        with torch.no_grad():
+            roi_preds = torch.sigmoid(roi_model(imgs))
+            roi_thresh = torch.zeros_like(roi_preds)
+            roi_thresh[roi_preds>0.5] = 1.0
+
+            roi_results = imgs * roi_thresh
+
+            mask_pred = net(roi_results)
+            mask_pred = torch.sigmoid(mask_pred)
+
+        thresh = torch.zeros_like(mask_pred)
+        thresh[mask_pred > 0.5] = 1.0
+
+        precision += precision_score(thresh,true_masks)
+        recall += recall_score(thresh,true_masks)
+        dice += dice_score(thresh,true_masks)
+        
+
+    print("dice score : {:.4f}, len(val_loader) : {:.4f}".format(dice, len(val_loader)))
+    print("dice score : {:.4f}, recall score : {:.4f}, precision score : {:.4f}".format(dice/len(val_loader), recall/len(val_loader),precision/len(val_loader)) )
         
 
 if __name__ == '__main__':
@@ -159,7 +199,7 @@ if __name__ == '__main__':
     set_seed(Model_SEED)
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device(f'cuda:4' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
     
     # parameters = {
@@ -168,14 +208,22 @@ if __name__ == '__main__':
     #         "gpu_number":2,
     #         "post_processing_dim": 256,
     #         "num_classes": 1
-    #     }ㅜ
+    #     }
 
-    # net = GMIC_UNet(parameters).to(device=device)
-
+    #net = GMIC_UNet(parameters).to(device=device)
     #net = pcca_UNet(1,1).to(device=device)
-    net = top_t_cbam_UNet(1,1,percent_t=0.3).to(device=device)
+    net = top_t_cbam_UNet(1,1,percent_t=0.5).to(device=device)
+    #net = LK_PC_UNet(1, 1).to(device=device)
     if torch.cuda.device_count() > 1:
-        net = nn.DataParallel(net,device_ids=[0,1,2,4]) 
+        net = nn.DataParallel(net,device_ids=[0,1,2]) 
 
-    train_net(net=net,device=device)
+    model_path = '/workspace/IITP/task_2D/dir_checkpoint_breast_ROI/top_t_cbam_UNet2.pth'
+
+    roi_model = top_t_cbam_UNet(1,1,percent_t=0.1).to(device="cuda:4")
+    if torch.cuda.device_count() > 1:
+        roi_model = nn.DataParallel(roi_model,device_ids=[0,1,2]) 
+
+    roi_model.load_state_dict(torch.load(model_path))
+
+    train_net(net=net,roi_model=roi_model,device=device)
 
